357dfe0a4eb8e74e1a9d1c338fb9b29290e99d58
DanielOuteiro <danieloiteiro@gmail.com>
2023-03-02 14:13:23 +0100

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -365,8 +365,8 @@ with tab2:
     date_range = format_date_range(start_date, end_date)
 
     # Add the strike range pickers
-    strike_abs_start = st.sidebar.text_input('Strike Abs Start', value='')
-    strike_abs_end = st.sidebar.text_input('Strike Abs End', value='')
+    strike_abs_start = st.sidebar.text_input('Only strikes above:', value='')
+    strike_abs_end = st.sidebar.text_input('Only strikes under:', value='')
     if strike_abs_start and strike_abs_end:
         params['strikeAbsRange'] = f"{strike_abs_start};{strike_abs_end}"
 
